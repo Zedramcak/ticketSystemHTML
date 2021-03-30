bdc64af3186896d17b4de34e8622fb59db6c86e2
Adam Zrcek <adamzrc@gmail.com>
2021-03-30 11:26:16 +0200

    added function to find all roles

--- src/main/java/eu/adamzrc/ticketSystemHTML/service/IRoleService.java
+++ src/main/java/eu/adamzrc/ticketSystemHTML/service/IRoleService.java
@@ -2,9 +2,12 @@ package eu.adamzrc.ticketSystemHTML.service;
 
 import eu.adamzrc.ticketSystemHTML.models.Role;
 
+import java.util.List;
+
 /**
  * Created by Adam Zrcek on 28.03.2021
  */
 public interface IRoleService {
     Role findRoleByName(String name);
+    List<Role> findAll();
 }
--- src/main/java/eu/adamzrc/ticketSystemHTML/service/RoleService.java
+++ src/main/java/eu/adamzrc/ticketSystemHTML/service/RoleService.java
@@ -5,6 +5,8 @@ import eu.adamzrc.ticketSystemHTML.repositories.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 /**
  * Created by Adam Zrcek on 28.03.2021
  */
@@ -12,6 +14,11 @@ import org.springframework.stereotype.Service;
 public class RoleService implements IRoleService{
     @Autowired
     RoleRepository repository;
+
+    @Override
+    public List<Role> findAll() {
+        return (List<Role>) repository.findAll();
+    }
 
     @Override
     public Role findRoleByName(String name) {
