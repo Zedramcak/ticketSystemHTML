7175ba19c5db0d0d5392b2b75eb9d47681a0a891
Adam Zrcek <adamzrc@gmail.com>
2021-03-30 11:25:51 +0200

    clean imports in UserDetailsService

--- src/main/java/eu/adamzrc/ticketSystemHTML/security/SecurityUserDetailsService.java
+++ src/main/java/eu/adamzrc/ticketSystemHTML/security/SecurityUserDetailsService.java
@@ -1,14 +1,7 @@
 package eu.adamzrc.ticketSystemHTML.security;
 
 import eu.adamzrc.ticketSystemHTML.models.User;
-import eu.adamzrc.ticketSystemHTML.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import javax.transaction.Transactional;
 
 /**
  * Created by Adam Zrcek on 27.03.2021
